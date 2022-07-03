ba09a6c0ec8bfbe9922df88418fb44d144ba86ff
CHOIY3SUNG <choiyesung2848@naver.com>
2022-07-04 04:00:01 +0900

    finish make banner

---
+++ MyAssets/MyAssets/Entities/AssetBanner.swift
@@ -0,0 +1,15 @@
+//
+//  AssetBanner.swift
+//  MyAssets
+//
+//  Created by Y3SUNG on 2022/07/04.
+//
+
+import Foundation
+import UIKit
+
+struct AssetBanner {
+    let title: String
+    let description: String
+    let backgroundColor: UIColor
+}
