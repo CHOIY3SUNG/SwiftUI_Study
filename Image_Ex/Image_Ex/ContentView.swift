e04996d107d3d0b4cb2a6660ed9057f4673018eb
CHOIY3SUNG <choiyesung2848@naver.com>
2022-07-18 02:01:49 +0900

    commit

--- Image_Ex/Image_Ex/ContentView.swift
+++ Image_Ex/Image_Ex/ContentView.swift
@@ -2,15 +2,21 @@
 //  ContentView.swift
 //  Image_Ex
 //
-//  Created by Y3SUNG on 2022/07/17.
+//  Created by Y3SUNG on 2022/07/17
 //
 
 import SwiftUI
+import MapKit
 
 struct ContentView: View {
+    @State private var viewModel = ContentViewModel()
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        Map(coordinateRegion: $viewModel.region, showsUserLocation: true)
+            .ignoresSafeArea()
+            .onAppear {
+                viewModel.checkIfLocationServicesIsEnabled()
+            }
     }
 }
 
@@ -18,4 +24,45 @@ struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
+}
+
+final class ContentViewModel: NSObject, ObservableObject, CLLocationManagerDelegate {
+    
+    @Published var region = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 37.75445232121803, longitude: 126.77256059813648), span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01))
+
+    
+    var locationManager: CLLocationManager?
+    
+    func checkIfLocationServicesIsEnabled() {
+        if CLLocationManager.locationServicesEnabled() {
+            locationManager = CLLocationManager()
+            locationManager!.delegate = self
+        } else {
+            print("Show an alert letting them...")
+        }
+    }
+    
+    private func checkLocationAuthorization() {
+        guard let locationManager = locationManager else { return }
+        
+        switch locationManager.authorizationStatus {
+            case .notDetermined:
+                locationManager.requestWhenInUseAuthorization()
+            case .restricted:
+                print("Your location is restricted likely due to parental controls.")
+            case .denied:
+                print("Your location is restricted likely due to parental controls.")
+            case .authorizedAlways, .authorizedWhenInUse:
+            region = MKCoordinateRegion(center: locationManager.location!.coordinate, span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01))
+            @unknown default:
+                break
+        }
+
+    }
+    
+    func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
+        checkLocationAuthorization()
+    }
+    
+    
 }
--- Image_Ex/Image_Ex/UserisWhere.swift
+++ Image_Ex/Image_Ex/UserisWhere.swift
@@ -5,4 +5,44 @@
 //  Created by Y3SUNG on 2022/07/18.
 //
 
-import Foundation
+import UIKit
+import CoreLocation
+
+class ViewController: UIViewController, CLLocationManagerDelegate {
+    
+    var locationManger = CLLocationManager()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // 델리게이트 설정
+        locationManger.delegate = self
+        // 거리 정확도 설정
+        locationManger.desiredAccuracy = kCLLocationAccuracyBest
+        // 사용자에게 허용 받기 alert 띄우기
+        locationManger.requestWhenInUseAuthorization()
+  
+        // 아이폰 설정에서의 위치 서비스가 켜진 상태라면
+        if CLLocationManager.locationServicesEnabled() {
+            print("위치 서비스 On 상태")
+         locationManger.startUpdatingLocation() //위치 정보 받아오기 시작
+        } else {
+            print("위치 서비스 Off 상태")
+        }
+        
+    }
+    
+    // 위치 정보 계속 업데이트 -> 위도 경도 받아옴
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        print("didUpdateLocations")
+        if let location = locations.first {
+            print("위도: \(location.coordinate.latitude)")
+            print("경도: \(location.coordinate.longitude)")
+        }
+    }
+    
+    // 위도 경도 받아오기 에러
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print(error)
+    }
+
+}
